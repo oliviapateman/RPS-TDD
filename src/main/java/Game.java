7bcce07cae212614ba94626d091e50aca4b178c5
Olivia Pateman <olivia.pateman@ten10.com>
2025-01-09 09:04:38 +0000

    Added event listener

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,25 +1,43 @@
 public class Game {
+
+    public interface EventListener{
+
+        void playerChoseMove(Player player, int move);
+        void playerWins(Player winner);
+        void draw(Boolean isADraw);
+        void endOfRound(Boolean endOfRound);
+    }
+
     private final Player player1;
     private final Player player2;
+    private final EventListener eventListener;
     private Player winner;
 
-    public Game(Player player1, Player player2) {
+    public Game(Player player1, Player player2, EventListener eventListener) {
         this.player1 = player1;
         this.player2 = player2;
+        this.eventListener = eventListener;
     }
 
     public void play() {
-        int player1Move = player1.chooseMove();
-        int player2Move = player2.chooseMove();
+        while (winner==null) {
+            int player1Move = player1.chooseMove();
+            int player2Move = player2.chooseMove();
 
-        if (Move.beats(player1Move, player2Move)) {
-            winner = player1;
-        } else if (Move.beats(player2Move, player1Move)) {
-            winner = player2;
+            eventListener.playerChoseMove(player1, player1Move);
+            eventListener.playerChoseMove(player2, player2Move);
+
+            if (Move.beats(player1Move, player2Move)) {
+                winner = player1;
+                eventListener.playerWins(winner);
+            } else if (Move.beats(player2Move, player1Move)) {
+                winner = player2;
+                eventListener.playerWins(winner);
+            } else {
+                eventListener.draw(true);
+            }
         }
+        eventListener.endOfRound(true);
     }
 
-    public Player getWinner() {
-        return winner;
-    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,29 +1,115 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GameTest {
     private final Player playsRock = new ConstantMovePlayer(Move.ROCK);
     private final Player playsScissors = new ConstantMovePlayer(Move.SCISSORS);
+    private final Player playsRockThenScissors = new TwoMovePlayer(-1);
+    SpyEventListener eventListener = new SpyEventListener();
+
+    @Test
+    void drawShouldReplay(){
+        Game game = new Game(playsRockThenScissors, playsRock, eventListener);
+
+        game.play();
+
+        eventListener.assertReportedReplay();
+    }
+
+    @Test
+    void shouldReportADraw(){
+        Game game = new Game(playsRockThenScissors, playsRock, eventListener);
+
+        game.play();
+
+        eventListener.assertReportedDraw();
+    }
+
+    @Test
+    void player2MoveShouldReportScissors() {
+        Game game = new Game(playsRock, playsScissors, eventListener);
+
+        game.play();
+
+        eventListener.assertReportedPlayerMove(playsScissors, Move.SCISSORS);
+    }
+
+    @Test
+    void player1MoveShouldReportRock() {
+        Game game = new Game(playsRock, playsScissors, eventListener);
+
+        game.play();
+
+        eventListener.assertReportedPlayerMove(playsRock, Move.ROCK);
+    }
 
     @Test
     void player1ShouldWin() {
         //Arrange
-        Game game = new Game(playsRock, playsScissors);
+        Game game = new Game(playsRock, playsScissors, eventListener);
         //Act
         game.play();
         //Assert
-        assertEquals(playsRock, game.getWinner());
+        eventListener.assertReportedWinner(playsRock);
     }
 
     @Test
     void player2ShouldWin() {
         //Arrange
-        Game game = new Game(playsScissors, playsRock);
+        Game game = new Game(playsScissors, playsRock, eventListener);
         //Act
         game.play();
         //Assert
-        assertEquals(playsRock, game.getWinner());
+        eventListener.assertReportedWinner(playsRock);
+    }
+
+    private static class SpyEventListener implements Game.EventListener {
+        private final Map<Player, Integer> reportedPlayerAndMove = new HashMap<>();
+        private Player reportedWinner;
+        private Boolean isADraw;
+        private Boolean endOfRound;
+
+        @Override
+        public void playerChoseMove(Player player, int move) {
+            reportedPlayerAndMove.put(player, move);
+        }
+
+        @Override
+        public void playerWins(Player winner) {
+            reportedWinner=winner;
+        }
+
+        @Override
+        public void draw(Boolean isADraw) {
+            this.isADraw = isADraw;
+        }
+
+        @Override
+        public void endOfRound(Boolean endOfRound) {
+            this.endOfRound=endOfRound;
+        }
+
+        private void assertReportedPlayerMove(Player expectedPlayer, int expectedMove) {
+            assertTrue(reportedPlayerAndMove.containsKey(expectedPlayer));
+            assertEquals(expectedMove, reportedPlayerAndMove.get(expectedPlayer));
+        }
+
+        private void assertReportedWinner(Player expectedPlayer){
+            assertEquals(expectedPlayer, reportedWinner);
+        }
+
+        private void assertReportedDraw(){
+            assertTrue(isADraw);
+        }
+
+        private void assertReportedReplay(){
+            assertTrue(isADraw);
+            assertTrue(endOfRound);
+        }
     }
 
     private static class ConstantMovePlayer implements Player {
@@ -44,6 +130,20 @@ public class GameTest {
             if (move == Move.PAPER) return "PlaysPaper";
             if (move == Move.SCISSORS) return "PlaysScissors";
             return super.toString();
+        }
+    }
+
+    private static class TwoMovePlayer implements Player {
+
+        private int move;
+
+        public TwoMovePlayer(int move) {
+            this.move = move;
+        }
+
+        @Override
+        public int chooseMove() {
+            return move+=1;
         }
     }
 }
