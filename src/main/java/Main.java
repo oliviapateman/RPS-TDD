b1e3a568e68e3bc86b854ce7645b8ba6d54024ca
Olivia Pateman <olivia.pateman@ten10.com>
2025-01-13 15:34:40 +0000

    Removed comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,7 @@ public class Main {
         PrintStream printStream = new PrintStream(System.out);
         Player humanPlayer = new HumanPlayer(printStream, new Scanner(System.in));
         Player computerPlayer = new ComputerPlayer(new Random());
-        //Game game = new Game(humanPlayer, computerPlayer, new PrintingEventListener(printStream));
         Tournament tournament = new Tournament(humanPlayer, computerPlayer, new PrintingEventListener(printStream));
         tournament.doThreeRounds();
-
-        //page 81
     }
 }
